a184591019925e9214b8821f4ca30095129ab113
Jaisman <jaismanjhinger@gmail.com>
2025-05-23 15:01:30 +0530

    /./

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 load_dotenv() 
-import spacy
 import numpy as np
 from rapidfuzz import process
-nlp = spacy.load("en_core_web_sm")
 app = Flask(__name__)
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
@@ -113,31 +111,32 @@ def about():
 @app.route('/docs')
 def docs():
     return render_template('doctor.html')
+
+
+import re
+
+
 def generate_ngrams(text, n=3):
-    words = text.split()
+    words = re.findall(r'\w+', text.lower())
     ngrams = []
-    
     for i in range(len(words)):
         for j in range(i+1, min(i+n, len(words))+1):
             ngrams.append(" ".join(words[i:j]))
-    
     return ngrams
 
 def match_symptoms(user_input, symptoms_list):
-    user_input = user_input.lower()
-    user_doc = nlp(user_input)
     user_phrases = generate_ngrams(user_input, n=3)
-
     matched_symptoms = set()
 
     for phrase in user_phrases:
         result = process.extractOne(phrase, symptoms_list)
-        if result:
+        if result and len(result) >= 2:
             best_match, score = result[0], result[1]
-            if score > 90:
+            if score>90:
                 matched_symptoms.add(best_match)
 
     return list(matched_symptoms)
+
 
 @app.route('/predict', methods = ['POST'])
 def predict():
@@ -154,7 +153,6 @@ def predict():
     prediction = model.predict(input_vector)
     predicted_index = np.argmax(prediction, axis=1)[0]
     predicted_disease = label_encoder.inverse_transform([predicted_index])[0]
-
     return render_template("result.html", disease=predicted_disease, matched_symptoms=matched_symptoms)
 
 if __name__ == "__main__":
